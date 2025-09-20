48ad3612b5580861a1921057e2c3ad324d81fb92
AlexLangan <alextrentlangan@gmail.com>
2025-09-20 09:52:07 +0100

    Initial project setup

---
+++ src/test/java/com/example/carmaintenance/CarMaintenanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMaintenanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
