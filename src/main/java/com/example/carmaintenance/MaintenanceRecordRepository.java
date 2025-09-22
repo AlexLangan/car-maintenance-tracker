3a6dc5d79ce48976e9298dac5f3a47f35b439b82
AlexLangan <alextrentlangan@gmail.com>
2025-09-22 20:14:56 +0100

    Save current changes before pulling

--- src/main/java/com/example/carmaintenance/MaintenanceRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.carmaintenance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaintenanceRecordRepository extends JpaRepository<MaintenanceRecord, Long> {
-
-}
